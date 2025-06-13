783a547fc6049ad72b3fd7d1b0ed35682c70c497
yubko18 <yubko18@gmail.com>
2025-06-14 02:50:05 +0600

    update files

--- src/shared/theme/index.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme, responsiveFontSizes } from "@mui/material/styles";
-
-export const theme = responsiveFontSizes(createTheme({
-  typography: {
-    htmlFontSize: 16,
-  },
-}));
